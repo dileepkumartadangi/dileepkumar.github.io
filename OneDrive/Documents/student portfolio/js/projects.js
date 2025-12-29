1ea8a42d1839aafd50025c3ac935e2da74fc88cf
dileepkumartadangi <151521329+dileepkumartadangi@users.noreply.github.com>
2025-12-29 17:41:53 +0530

    Rename image folder to images

---
+++ OneDrive/Documents/student portfolio/js/projects.js
@@ -0,0 +1,25 @@
+function openCertificate() {
+  document.getElementById("certificateModal").style.display = "flex";
+}
+
+function closeCertificate() {
+  document.getElementById("certificateModal").style.display = "none";
+}
+
+function openAIMLCertificate() {
+  document.getElementById("aiMlCertificateModal").style.display = "flex";
+}
+
+function closeAIMLCertificate() {
+  document.getElementById("aiMlCertificateModal").style.display = "none";
+}
+
+function openWebDevCertificate() {
+  document.getElementById("webDevCertificateModal").style.display = "flex";
+}
+
+function closeWebDevCertificate() {
+  document.getElementById("webDevCertificateModal").style.display = "none";
+}
+
+
